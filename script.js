7e3cf09f42b71daa62e1231d22e1039d577b8a0a
seranbaskaran <110149246+seranbaskaran@users.noreply.github.com>
2023-10-06 19:41:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var container = document.querySelector(".container"),
+      signUp = document.querySelector(".signup-link"),
+      login = document.querySelector(".login-link");
+
+
+      
+    signUp.addEventListener("click", ( )=>{
+        container.classList.add("active");
+    });
+    login.addEventListener("click", ( )=>{
+        container.classList.remove("active");
+    });
+    
